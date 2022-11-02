e6e20e368c007ac6b8e1b944da50a0b22591d20e
Tien <73010955+tientran505@users.noreply.github.com>
2022-11-02 21:43:22 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+*   
+*   Created by ${USER}
+*   Date ${DATE} - ${TIME}
+*   Description: ...
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
